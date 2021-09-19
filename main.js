c7a236dc4af8cad1e24b29761a3b656bfd16f4fc
Keegan <b.keegan12@yahoo.com>
2021-09-18 20:58:07 -0400

    update js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './style.css'
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / windows.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(windows.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
